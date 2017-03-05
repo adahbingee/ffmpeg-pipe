4834278d58fee9a0d685a9342400d6e910c9d6ef
adahbingee <adahbingee@gmail.com>
2017-03-05 21:25:57 +0800

    optimize js

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ let imgSizeX = 1920;
 let imgSizeY = 1080;
 let imgNum   = 1000;
 
-let t1 = Date.now();
-
 let data = Buffer.alloc(imgSizeX*imgSizeY*3);
 
 for (var i = 0; i < imgNum; i+=1) {
@@ -16,8 +14,5 @@ for (var i = 0; i < imgNum; i+=1) {
             data[offset+2] = 0x00;
         }
     }
-    process.stdout.write( data );
+    process.stderr.write( data );
 }
-
-let t2 = Date.now();
-process.stderr.write(`duration: ${t2-t1}`);
